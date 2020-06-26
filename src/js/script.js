cdb47e57b3c6a7e6ee7551f2be2c01b0b07b728c
Alex <alexlong2001@outlook.com>
2020-06-26 22:00:03 +0100

    Projects section updated

--- src/js/script.js
+++ src/js/script.js
@@ -1,42 +1,63 @@
 const children = document.querySelectorAll('section')
 const about = document.querySelector('.about');
-about.style.display = 'none';
 const servicesImg = document.querySelector('.services-img');
 const servicesText = document.querySelector('.services-list');
-servicesImg.style.display = 'none';
-const leftProject = document.querySelectorAll('.left');
-const rightProject = document.querySelectorAll('.right');
-leftProject.forEach(project => {
-  project.style.display = 'none';
-});
-rightProject.forEach(project => {
-  project.style.display = 'none';
-})
-console.log(leftProject);
-const options = {};
+const buildadream = document.querySelector('.buildadream');
+const rps = document.querySelector('.rps');
+const hosting = document.querySelector('.hosting');
+const quiz = document.querySelector('.quiz');
+
+let t = 0;
+const message = 'SEND A MESSAGE';
+const contactTitle = document.querySelector('.contactTitle h2');
+let programRan = false;
+
+const typeWriter = type => {
+  if (t < type.length) {
+    contactTitle.innerHTML += type.charAt(t);
+    t++;
+    setTimeout(typeWriter, 40, type);
+    programRan = true;
+  }
+};
+
+const options = {
+  threshold: .3
+}
 
 const observer = new IntersectionObserver((entries, observer) => {
   entries.forEach(entry => {
-    if(entry.isIntersecting && entry.target.classList.contains('aboutSection')) {
-      about.style.display = '';
+    if(entry.isIntersecting) {
+      if(entry.target.classList.contains('aboutSection')) {
+      about.style.display = 'block';
       about.style.animation = '1.5s ease-in-out slideLeft forwards';
-    }
-    if(entry.isIntersecting && entry.target.classList.contains('servicesSection')) {
-      servicesImg.style.display = '';
-      servicesImg.style.animation = '1s ease-in-out up forwards';
-      servicesText.style.animation = '1s .3s ease-in-out up forwards';
-    }
-    if(entry.isIntersecting && entry.target.classList.contains('projectSection')) {
-      leftProject.forEach(project => {
-        project.style.display = '';
-      });
-      rightProject.forEach(project => {
-        project.style.display = '';
-      })
-      leftProject[0].style.animation = '1s ease-in-out forwards slideLeft';
-      rightProject[0].style.animation = '1s .25s ease-in-out forwards slideRight';
-      leftProject[1].style.animation = '1s .5s ease-in-out forwards slideLeft';
-      rightProject[1].style.animation = '1s .75s ease-in-out forwards slideRight';
+      }
+      if(entry.target.classList.contains('servicesSection')) {
+        servicesImg.style.display = 'flex';
+        servicesImg.style.animation = '1s ease-in-out up forwards';
+        servicesText.style.animation = '1s .3s ease-in-out up forwards';
+      }
+      if(entry.target.classList.contains('buildSection')) {
+        buildadream.style.display = 'flex';
+        buildadream.style.animation = '1s ease-in-out forwards slideLeft';
+      }
+      if(entry.target.classList.contains('rpsSection')) {
+        rps.style.display = 'flex';
+        rps.style.animation = '1s ease-in-out forwards slideRight';
+      }
+      if(entry.target.classList.contains('hostingSection')) {
+        hosting.style.display = 'flex';
+        hosting.style.animation = '1s ease-in-out forwards slideLeft';
+      }
+      if(entry.target.classList.contains('quizSection')) {
+        quiz.style.display = 'flex';
+        quiz.style.animation = '1s ease-in-out forwards slideRight';
+      }
+      if(entry.target.classList.contains('contactSection')) {
+        if (!programRan) {
+          typeWriter(message);
+        } 
+      }
     }
   })
 }, options);
@@ -45,22 +66,3 @@ children.forEach(child => {
   observer.observe(child);
 });
 
-
-/*
-  let t = 0;
-  const message = 'SEND A MESSAGE';
-  const contactTitle = document.querySelector('.contactTitle h2');
-  let programRan = false;
-
-  const typeWriter = type => {
-    if (t < type.length) {
-      contactTitle.innerHTML += type.charAt(t);
-      t++;
-      setTimeout(typeWriter, 40, type);
-      programRan = true;
-    }
-  };
-  if (!programRan) {
-    typeWriter(message);
-  }
-*/
